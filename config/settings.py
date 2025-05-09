ea29e56bcee5e26df5d2b40c0c70aaa8a29d3fc4
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-09 19:05:50 +0500

    the end

--- config/settings.py
+++ config/settings.py
@@ -1,23 +1,18 @@
 
+from decouple import config
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2n3$_ptudc9ivtm&%+=yf_e_^eeeohtlsuqo)@sp=9eq)3&#_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
